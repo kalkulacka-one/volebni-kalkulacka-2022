de182297e302e16d2e122ea33de287dfd561ce23
Martin Wenisch <martin.wenisch@cesko.digital>
2022-05-10 17:36:55 +0200

    Test deployment

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,12 @@ handler = Mangum(app)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World!"}
+    return {"Hello": "World!!"}
 
 @app.get("/test")
 def test1():
     return {"Test": "Test1"}
+
+@app.get("/test2")
+def test2():
+    return {"Test": "Test2"}
