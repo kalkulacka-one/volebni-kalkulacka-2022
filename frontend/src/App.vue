ac2dd1a53d52f3607cb9622d206f327301edb9b3
Ondrej Kyjanek <ondrej.kyjanek@email.cz>
2022-08-08 11:12:01 +0200

    VK-84 simple index

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,45 +4,19 @@ import TheWelcome from './components/TheWelcome.vue';
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <IndexPage/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import IndexPage from './components/IndexPage.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+export default {
+  name: "App",
+  components: {
+    IndexPage,
+},
+};
+</script>
 
   header .wrapper {
     display: flex;
