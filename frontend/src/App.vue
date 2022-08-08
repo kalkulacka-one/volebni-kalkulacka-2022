fe81b8bce314b814485ab7c93e119ebbd64ad60a
Ondrej Kyjanek <ondrej.kyjanek@email.cz>
2022-08-08 16:48:21 +0200

    VK-84 icon buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';
+import IndexPage from './components/IndexPage.vue';
 </script>
 
 <template>
-  <IndexPage/>
+  <IndexPage />
 </template>
-
-<script>
-import IndexPage from './components/IndexPage.vue';
-
-export default {
-  name: "App",
-  components: {
-    IndexPage,
-},
-};
-</script>
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
