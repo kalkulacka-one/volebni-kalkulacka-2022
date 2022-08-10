a676daddf5ffbf0a870f05d18a663da73ac5a5f3
Martin Wenisch <martin.wenisch@cesko.digital>
2022-08-10 11:55:17 +0200

    NewRelic log ingestion (#9)
    * NewRelic log ingestion
    
    * Format
    
    * Update infrastructure/aws/newrelic.tf
    
    Co-authored-by: Martin Majlis <martin@majlis.cz>
    
    Co-authored-by: Martin Majlis <martin@majlis.cz>

---
+++ infrastructure/aws/newrelic.tf
@@ -0,0 +1,10 @@
+/*
+  Log ingestion for New Relic platform.
+*/
+
+module "newrelic_log_ingestion" {
+  source             = "github.com/newrelic/aws-log-ingestion"
+  nr_license_key     = var.newrelic-license-key
+  nr_tags            = "project:volebni-kalkulacka"
+  nr_logging_enabled = true
+}
