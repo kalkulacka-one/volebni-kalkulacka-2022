db2e8d4033b6e6b7e033a1f51495513ebea01187
Kryštof Korb <krystof@korb.cz>
2022-08-15 23:35:05 +0200

    Add icon to link button

--- frontend/src/components/design-system/buttons/LinkButton.vue
+++ frontend/src/components/design-system/buttons/LinkButton.vue
@@ -15,8 +15,26 @@ import ButtonText from '@/components/design-system/typography/ButtonText.vue';
 
 <template>
   <SimpleButton :tag="tag" :href="href">
+    <span
+      v-if="$slots.icon"
+      :class="['icon', $slots.default ? 'icon--before' : null]"
+    >
+      <slot name="icon" />
+    </span>
     <ButtonText v-if="$slots.default">
       <slot />
     </ButtonText>
+    <span v-if="$slots.iconAfter" :class="['icon', 'icon--after']">
+      <slot name="iconAfter" />
+    </span>
   </SimpleButton>
 </template>
+
+<style scoped lang="scss">
+.icon--before {
+  margin-right: var(--spacing-extra-small);
+}
+.icon--after {
+  margin-left: var(--spacing-extra-small);
+}
+</style>
