9aca6560bf2da2735e027f1ecf8c34110b290534
Kryštof Korb <krystof@korb.cz>
2022-08-15 21:46:18 +0200

    Add simple button component

---
+++ frontend/src/components/design-system/buttons/SimpleButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+export interface Props {
+  tag?: 'button' | 'a';
+  href?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  tag: 'button',
+  href: undefined,
+});
+</script>
+
+<template>
+  <component :is="tag" :href="href">
+    <slot />
+  </component>
+</template>
