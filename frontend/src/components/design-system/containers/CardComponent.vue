3946e982e7f84993b2035ab63e9aa7987f1ae21b
Kryštof Korb <krystof@korb.cz>
2022-08-18 22:56:51 +0200

    Add card component interface

---
+++ frontend/src/components/design-system/containers/CardComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+export interface Props {
+  radius?: 'small' | 'large';
+  corner: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+  border?: boolean;
+  borderKind?: 'normal' | 'strong';
+  shadow?: boolean;
+  padding?: 'small' | 'large';
+}
+
+withDefaults(defineProps<Props>(), {
+  radius: 'small',
+  border: false,
+  borderKind: 'normal',
+  shadow: false,
+  padding: 'small',
+});
+</script>
+
+<template>
+  <slot />
+</template>
