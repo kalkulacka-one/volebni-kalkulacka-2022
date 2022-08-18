34bc38aafbf3e662dfc53310d933d3179bfa4510
Kryštof Korb <krystof@korb.cz>
2022-08-18 23:37:18 +0200

    Add card component styles

--- frontend/src/components/design-system/containers/CardComponent.vue
+++ frontend/src/components/design-system/containers/CardComponent.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 export interface Props {
   radius?: 'small' | 'large';
   corner: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
@@ -8,15 +10,86 @@ export interface Props {
   padding?: 'small' | 'large';
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   radius: 'small',
   border: false,
   borderKind: 'normal',
   shadow: false,
   padding: 'small',
 });
+
+const classes = computed(() => ({
+  [`radius--${props.radius}`]: props.radius,
+  [`corner--${props.corner}`]: props.corner,
+  border: props.border,
+  [`border--${props.borderKind}`]: props.border,
+  shadow: props.shadow,
+  [`padding--${props.padding}`]: props.padding,
+}));
 </script>
 
 <template>
-  <slot />
+  <div :class="classes">
+    <slot />
+  </div>
 </template>
+
+<style scoped lang="scss">
+div {
+  background-color: rgb(var(--color-neutral-bg-container));
+}
+
+.radius {
+  &--small {
+    border-radius: var(--radius-small);
+  }
+  &--large {
+    border-radius: var(--radius-large);
+  }
+}
+
+.corner {
+  &--top-left {
+    border-top-left-radius: 0;
+  }
+  &--top-right {
+    border-top-right-radius: 0;
+  }
+  &--bottom-left {
+    border-bottom-left-radius: 0;
+  }
+  &--bottom-right {
+    border-bottom-right-radius: 0;
+  }
+}
+
+.border {
+  border-style: solid;
+  border-width: var(--spacing-pico);
+
+  &--normal {
+    border-color: rgb(var(--color-neutral-border));
+  }
+
+  &--strong {
+    border-color: rgb(var(--color-neutral-border-strong));
+  }
+}
+
+.shadow {
+  box-shadow: 6px 8px 0px 0px
+    rgba(var(--color-neutral-shadow), var(--transparency-20));
+}
+
+.padding {
+  &--small {
+    padding: var(--spacing-small);
+  }
+  &--large {
+    padding-top: var(--spacing-large);
+    padding-bottom: var(--spacing-large);
+    padding-left: calc(2 * var(--spacing-large));
+    padding-right: calc(2 * var(--spacing-large));
+  }
+}
+</style>
