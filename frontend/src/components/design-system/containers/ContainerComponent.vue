752f83917c24532435700d31b2864b29677a230a
Kryštof Korb <krystof@korb.cz>
2022-08-21 00:23:35 +0200

    Add container skeleton

---
+++ frontend/src/components/design-system/containers/ContainerComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+export interface Props {
+  padding?: 'small' | 'large';
+  background?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  padding: undefined,
+  background: 'rgb(var(--color-neutral-bg-container))',
+});
+</script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
