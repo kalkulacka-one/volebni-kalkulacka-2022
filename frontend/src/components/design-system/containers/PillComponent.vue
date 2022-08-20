918b2fda5f2269441f5ec753a215045f64b8a33d
Marcela Karafizievová <marcela.karafizievova@gmail.com>
2022-08-20 22:40:47 +0200

    Add pill skeleton

---
+++ frontend/src/components/design-system/containers/PillComponent.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+export enum ColorPill {
+  Light,
+  Dark,
+}
+</script>
+
+<script setup lang="ts">
+export interface Props {
+  icon: boolean;
+  color?: ColorPill;
+  title: string;
+}
+withDefaults(defineProps<Props>(), {
+  icon: false,
+  color: ColorPill.Light,
+  title: 'Label',
+});
+</script>
+
+<template>
+  <slot />
+</template>
