61b23c9f7c5e79cf99b312f4e439cc21e2880102
Marcela Karafizievová <62770219+CoffeeladyCZ@users.noreply.github.com>
2022-08-21 14:42:46 +0200

    VK-140 Add pill skeleton (#29)

---
+++ frontend/src/components/design-system/containers/PillComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LabelText from '@/components/design-system/typography/LabelText.vue';
+
+export interface Props {
+  active?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  active: false,
+});
+</script>
+
+<template>
+  <LabelText>
+    <slot />
+  </LabelText>
+</template>
