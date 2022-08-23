5f7feb81e2b9db013046ad669b2864334b9f2300
Kryštof Korb <krystof@korb.cz>
2022-08-23 18:12:51 +0200

    Add styles

--- frontend/src/components/design-system/containers/ContainerComponent.vue
+++ frontend/src/components/design-system/containers/ContainerComponent.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 export interface Props {
   padding?: boolean;
   paddingSize?: 'small' | 'large';
@@ -10,10 +12,27 @@ const props = withDefaults(defineProps<Props>(), {
   paddingSize: 'small',
   background: 'rgb(var(--color-neutral-bg-container))',
 });
+
+const classes = computed(() => ({
+  [`container--padding-${props.paddingSize}`]: props.padding,
+}));
 </script>
 
 <template>
-  <div>
+  <div :class="['container', classes]">
     <slot />
   </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+  background-color: v-bind(background);
+
+  &--padding-small {
+    padding: var(--spacing-small);
+  }
+  &--padding-large {
+    padding: var(--spacing-large);
+  }
+}
+</style>
