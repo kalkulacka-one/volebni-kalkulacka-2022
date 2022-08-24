ca2879dd91ef33fa05ddc7c9338a4281f2722b61
Marcela Karafizievová <62770219+CoffeeladyCZ@users.noreply.github.com>
2022-08-24 22:15:55 +0200

    VK-133 - Add tag skeleton (#32)

---
+++ frontend/src/components/design-system/containers/TagComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import LabelText from '@/components/design-system/typography/LabelText.vue';
+</script>
+
+<template>
+  <LabelText>
+    <slot />
+  </LabelText>
+</template>
