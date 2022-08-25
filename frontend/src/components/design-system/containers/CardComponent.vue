49fff721f0f6988c991320914eaaa5d1ddbced6d
Kryštof Korb <krystof@korb.cz>
2022-08-25 14:31:01 +0200

    VK-114 Add card (#26)

---
+++ frontend/src/components/design-system/containers/CardComponent.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+export interface Props {
+  radius?: 'small' | 'large';
+  corner: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+  border?: boolean;
+  borderKind?: 'normal' | 'strong';
+  shadow?: boolean;
+  padding?: 'small' | 'large';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  radius: 'small',
+  border: false,
+  borderKind: 'normal',
+  shadow: false,
+  padding: 'small',
+});
+
+const classes = computed(() => ({
+  [`card--radius-${props.radius}`]: props.radius,
+  [`card--corner-${props.corner}`]: props.corner,
+  'card--border': props.border,
+  [`card--border-${props.borderKind}`]: props.border,
+  'card--shadow': props.shadow,
+  [`card--padding-${props.padding}`]: props.padding,
+}));
+</script>
+
+<template>
+  <div :class="['card', classes]">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  background-color: rgb(var(--color-neutral-bg-container));
+
+  &--radius {
+    &-small {
+      border-radius: var(--radius-small);
+    }
+
+    &-large {
+      border-radius: var(--radius-large);
+    }
+  }
+
+  &--corner {
+    &-top-left {
+      border-top-left-radius: 0;
+    }
+
+    &-top-right {
+      border-top-right-radius: 0;
+    }
+
+    &-bottom-left {
+      border-bottom-left-radius: 0;
+    }
+
+    &-bottom-right {
+      border-bottom-right-radius: 0;
+    }
+  }
+
+  &--border {
+    border-style: solid;
+    border-width: var(--spacing-pico);
+
+    &-normal {
+      border-color: rgb(var(--color-neutral-border));
+    }
+
+    &-strong {
+      border-color: rgb(var(--color-neutral-border-strong));
+    }
+  }
+
+  &--shadow {
+    box-shadow: 6px 8px 0px 0px
+      rgba(var(--color-neutral-shadow), var(--transparency-20));
+  }
+
+  &--padding {
+    &-small {
+      padding: var(--spacing-small);
+    }
+
+    &-large {
+      padding-top: var(--spacing-large);
+      padding-bottom: var(--spacing-large);
+      padding-left: calc(2 * var(--spacing-large));
+      padding-right: calc(2 * var(--spacing-large));
+    }
+  }
+}
+</style>
