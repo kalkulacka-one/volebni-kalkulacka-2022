c42c3b658e144d822c60be77167f12a4c0205f5c
Kryštof Korb <krystof@korb.cz>
2022-08-27 10:42:46 +0200

    Add all buttons (#37)

--- frontend/src/components/design-system/icons/IconComponent.vue
+++ frontend/src/components/design-system/icons/IconComponent.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 export interface Props {
   icon: string;
   color: string;
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large' | 'extra-large';
   title?: string;
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   size: 'medium',
   color: 'currentColor',
   title: undefined,
 });
+
+const classes = computed(() => [`icon--${props.size}`]);
 </script>
 
 <template>
-  <span :class="size">
+  <div :class="['icon', classes]">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       role="img"
@@ -24,22 +28,29 @@ withDefaults(defineProps<Props>(), {
       <title v-if="title">{{ title }}</title>
       <path :d="icon" :fill="color" />
     </svg>
-  </span>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.small {
-  width: var(--spacing-small);
-  height: var(--spacing-small);
-}
+.icon {
+  &--small {
+    width: var(--spacing-small);
+    height: var(--spacing-small);
+  }
 
-.medium {
-  width: var(--spacing-medium);
-  height: var(--spacing-medium);
-}
+  &--medium {
+    width: var(--spacing-medium);
+    height: var(--spacing-medium);
+  }
 
-.large {
-  width: var(--spacing-large);
-  height: var(--spacing-large);
+  &--large {
+    width: var(--spacing-large);
+    height: var(--spacing-large);
+  }
+
+  &--extra-large {
+    width: var(--spacing-extra-large);
+    height: var(--spacing-extra-large);
+  }
 }
 </style>
