61a34b1deebec00db8e0a473f3a2a63303e990a2
Kryštof Korb <krystof@korb.cz>
2022-08-27 14:18:46 +0200

    Add background component skeleton

---
+++ frontend/src/components/design-system/style/BackgroundComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const classes = computed(() => ({}));
+</script>
+
+<template>
+  <div :class="['background', classes]">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.background {
+  background-color: var(--color-neutral-bg);
+}
+</style>
