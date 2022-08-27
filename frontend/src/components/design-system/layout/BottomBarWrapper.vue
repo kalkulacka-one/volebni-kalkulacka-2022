9f7f04d75bca1b5ac56ce0959faa9bd211440529
Kryštof Korb <krystof@korb.cz>
2022-08-27 13:54:23 +0200

    Add bottom bar wrapper component

---
+++ frontend/src/components/design-system/layout/BottomBarWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bottom-bar-wrapper">
+    <div class="main">
+      <slot />
+    </div>
+    <div class="bottom-bar">
+      <slot name="bottom-bar" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.bottom-bar-wrapper {
+  display: grid;
+  grid-template-rows: 1fr auto;
+  grid-template-areas: 'main' 'bottom-bar';
+
+  .main {
+    display: grid;
+    grid-area: main;
+  }
+
+  .bottom-bar {
+    grid-area: bottom-bar;
+    position: sticky;
+    bottom: 0;
+  }
+}
+</style>
