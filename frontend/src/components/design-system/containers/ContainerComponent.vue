dd4d9c620fafabd10c8ca73fedd9403858292c0c
Kryštof Korb <krystof@korb.cz>
2022-08-27 10:43:19 +0200

    VK-115 Style container (#36)

--- frontend/src/components/design-system/containers/ContainerComponent.vue
+++ frontend/src/components/design-system/containers/ContainerComponent.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 export interface Props {
   padding?: 'small' | 'large';
   background?: string;
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   padding: undefined,
   background: 'rgb(var(--color-neutral-bg-container))',
 });
+
+const classes = computed(() => ({
+  [`container--padding-${props.padding}`]: props.padding,
+}));
 </script>
 
 <template>
-  <div>
+  <div :class="['container', classes]">
     <slot />
   </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+  background-color: v-bind(background);
+
+  &--padding {
+    &-small {
+      padding: var(--spacing-small);
+    }
+    &-large {
+      padding: var(--spacing-large);
+    }
+  }
+}
+</style>
