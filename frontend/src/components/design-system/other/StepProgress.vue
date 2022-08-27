ae899b446a3d91b73c9a50d89527980d321b4617
Kryštof Korb <krystof@korb.cz>
2022-08-27 14:36:37 +0200

    Add step progress component skeleton

---
+++ frontend/src/components/design-system/other/StepProgress.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+export interface Props {
+  current: number;
+}
+
+defineProps<Props>();
+
+const totalSteps = 4;
+</script>
+
+<template>
+  <div class="step-progress">{{ current }} / {{ totalSteps }}</div>
+</template>
