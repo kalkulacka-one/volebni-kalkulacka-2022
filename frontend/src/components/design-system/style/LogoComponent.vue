d9a05b7210b2f0fd16a69a50689dff11ea829249
Kryštof Korb <krystof@korb.cz>
2022-08-28 20:04:39 +0200

    Add various skeletons (#46)

---
+++ frontend/src/components/design-system/style/LogoComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+export interface Props {
+  monochromatic?: string;
+  text?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  monochromatic: undefined,
+  text: true,
+});
+
+const classes = computed(() => ({
+  'logo--monochromatic': props.monochromatic,
+  'logo--text': props.text,
+}));
+</script>
+
+<template>
+  <div :class="['logo', classes]">
+    <div class="text">Volební kalkulačka</div>
+  </div>
+</template>
