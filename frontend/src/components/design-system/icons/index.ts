2d02c14117ccb7fcc70552e7d8e3ec10c3184df3
Kryštof Korb <krystof@korb.cz>
2022-09-05 00:12:26 +0200

    Add logo percent icon

--- frontend/src/components/design-system/icons/index.ts
+++ frontend/src/components/design-system/icons/index.ts
@@ -2,20 +2,22 @@ import vkiCheckbox from './vkiCheckbox';
 import vkiCheckboxChecked from './vkiCheckboxChecked';
 import vkiCheckboxIndeterminate from './vkiCheckboxIndeterminate';
 import vkiLogoAgainst from './vkiLogoAgainst';
+import vkiLogoCircle from './vkiLogoCircle';
 import vkiLogoInFavour from './vkiLogoInFavour';
 import vkiLogoNeutral from './vkiLogoNeutral';
+import vkiLogoPercent from './vkiLogoPercent';
 import vkiStarFilled from './vkiStarFilled';
 import vkiStarOutlined from './vkiStarOutlined';
-import vkiLogoCircle from './vkiLogoCircle';
 
 export {
   vkiCheckbox,
   vkiCheckboxChecked,
   vkiCheckboxIndeterminate,
   vkiLogoAgainst,
+  vkiLogoCircle,
   vkiLogoInFavour,
   vkiLogoNeutral,
+  vkiLogoPercent,
   vkiStarFilled,
   vkiStarOutlined,
-  vkiLogoCircle,
 };
