66cdf7b278ce1ca7bcc09c3613017b8589cfb906
Kryštof Korb <krystof@korb.cz>
2022-09-05 21:15:37 +0200

    Reimplement bottom bar on question page

--- frontend/src/routes/question/QuestionBottomBar.vue
+++ frontend/src/routes/question/QuestionBottomBar.vue
@@ -1,86 +1,109 @@
-<script lang="ts">
-export interface IButton {
-  onClick: () => void;
-  isSelected: boolean;
-}
-</script>
 <script setup lang="ts">
+import { computed } from 'vue';
+
 import { UserAnswerEnum, type UserAnswer } from '@/stores/electionStore';
-defineProps<{
-  questionCurrentNr: number;
-  questionProgress: number;
-  questionTotal: number;
+
+import BodyText from '@/components/design-system/typography/BodyText.vue';
+import BottomBar from '@/components/design-system/navigation/BottomBar.vue';
+import ButtonComponent from '@/components/design-system/input/ButtonComponent.vue';
+import IconButton from '@/components/design-system/input/IconButton.vue';
+import IconComponent from '@/components/design-system/icons/IconComponent.vue';
+import StackComponent from '@/components/design-system/layout/StackComponent.vue';
+
+import {
+  vkiLogoInFavour,
+  vkiLogoAgainst,
+  vkiLogoNeutral,
+  vkiStarOutlined,
+  vkiStarFilled,
+} from '@/components/design-system/icons';
+
+const props = defineProps<{
   answer: UserAnswer;
   starClick: () => void;
   yesClick: () => void;
   noClick: () => void;
   skipClick: () => void;
 }>();
+
+const starColor = computed(() =>
+  props.answer.flag ? 'rgba(var(--palette-yellow))' : undefined
+);
+const starIcon = computed(() =>
+  props.answer.flag ? vkiStarFilled : vkiStarOutlined
+);
+
+console.log(props.answer);
 </script>
 
 <template>
-  <div class="wrapper">
-    <progress
-      id="question-progress"
-      class="progress"
-      :value="questionProgress + 1"
-      :max="questionTotal"
-    ></progress>
-    <progress
-      id="question-current"
-      class="progress"
-      :value="questionCurrentNr + 1"
-      :max="questionTotal"
-    ></progress>
-    <div class="button-wrapper">
-      <button :class="answer.flag ? 'button--active' : ''" @click="starClick">
-        Pro me dulezite
-      </button>
-      <button
-        :class="answer.answer === UserAnswerEnum.yes ? 'button--active' : ''"
+  <BottomBar class="bottom-bar" transparent="never">
+    <StackComponent horizontal centered>
+      <IconButton
+        :icon="starIcon"
+        :color="starColor"
+        size="large"
+        title="Pro mě důležité"
+        @click="starClick"
+      />
+      <BodyText class="star-text" size="medium"> Pro mě důležité </BodyText>
+    </StackComponent>
+    <StackComponent horizontal spacing="small">
+      <ButtonComponent
+        class="in-favour"
+        kind="answer"
+        color="primary"
+        responsive
+        :selected="answer.answer === UserAnswerEnum.yes"
         @click="yesClick"
       >
+        <template #icon>
+          <IconComponent :icon="vkiLogoInFavour" />
+        </template>
         Jsem pro
-      </button>
-      <button
-        :class="answer.answer === UserAnswerEnum.no ? 'button--active' : ''"
+      </ButtonComponent>
+      <ButtonComponent
+        class="against"
+        kind="answer"
+        color="secondary"
+        responsive
+        :selected="answer.answer === UserAnswerEnum.no"
         @click="noClick"
       >
+        <template #icon>
+          <IconComponent :icon="vkiLogoAgainst" />
+        </template>
         Jsem proti
-      </button>
-      <button
-        :class="answer.answer === UserAnswerEnum.skip ? 'button--active' : ''"
-        @click="skipClick"
-      >
-        Preskocit
-      </button>
-    </div>
-  </div>
+      </ButtonComponent>
+    </StackComponent>
+    <ButtonComponent
+      class="skip"
+      kind="answer"
+      responsive
+      :selected="answer.answer === UserAnswerEnum.skip"
+      @click="skipClick"
+    >
+      <template #icon>
+        <IconComponent :icon="vkiLogoNeutral" />
+      </template>
+      Přeskočit
+    </ButtonComponent>
+  </BottomBar>
 </template>
 
-<style scoped lang="scss">
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0px;
-  gap: 5px;
-}
-.button-wrapper {
-  display: flex;
-  flex-direction: row;
+<style lang="scss" scoped>
+.bottom-bar {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  gap: var(--spacing-large);
   justify-content: center;
-  align-items: center;
-  padding: 0px;
-  gap: 24px;
-  width: 100%;
-}
-.progress {
-  width: 100%;
-}
-.button {
-  &--active {
-    background-color: magenta;
+
+  @media (max-width: 700px) {
+    justify-content: space-between;
+
+    .star-text {
+      display: none;
+    }
   }
 }
 </style>
