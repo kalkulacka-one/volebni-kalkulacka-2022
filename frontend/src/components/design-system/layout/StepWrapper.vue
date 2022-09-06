1e6b10a02a9cc6ceef1a8e54fad93a327a1a8ad4
Kryštof Korb <krystof@korb.cz>
2022-09-06 16:20:17 +0200

    VK-192 Update step wrapper (#69)

--- frontend/src/components/design-system/layout/StepWrapper.vue
+++ frontend/src/components/design-system/layout/StepWrapper.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="step-wrapper">
-    <slot name="before" />
-    <slot />
-    <slot name="after" />
+    <div class="before">
+      <slot name="before" />
+    </div>
+    <div class="main">
+      <slot />
+    </div>
+    <div class="after">
+      <slot name="after" />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .step-wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  display: grid;
+  grid-template-columns: 1fr clamp(32rem, 50vw, 48rem) 1fr;
+  grid-template-areas: 'before main after';
+  gap: var(--spacing-large);
   height: 100%;
+  align-items: center;
   padding: var(--spacing-small);
+
+  .before {
+    grid-area: before;
+    justify-self: end;
+  }
+
+  .main {
+    grid-area: main;
+  }
+
+  .after {
+    grid-area: after;
+  }
+
+  /* TODO: update breakpoint */
+  @media (max-width: 700px) {
+    grid-template-columns: 1fr;
+    grid-template-areas: 'main';
+
+    .before,
+    .after {
+      display: none;
+    }
+  }
 }
 </style>
