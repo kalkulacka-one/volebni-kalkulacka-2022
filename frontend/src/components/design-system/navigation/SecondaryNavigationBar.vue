2d533b6fce50a7b2c21806b38d10a1abc53399b7
Kryštof Korb <krystof@korb.cz>
2022-09-07 17:52:18 +0200

    Enhance secondary navigation bar

--- frontend/src/components/design-system/navigation/SecondaryNavigationBar.vue
+++ frontend/src/components/design-system/navigation/SecondaryNavigationBar.vue
@@ -6,11 +6,17 @@ import TitleText from '@/components/design-system/typography/TitleText.vue';
 
 export interface Props {
   transparent?: boolean;
+  centered?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
   transparent: false,
+  centered: false,
 });
+
+const classes = computed(() => ({
+  'secondary-navigation-bar--centered': props.centered,
+}));
 
 const background = computed(() =>
   props.transparent ? 'transparent' : undefined
@@ -19,7 +25,7 @@ const background = computed(() =>
 
 <template>
   <ContainerComponent
-    class="secondary-navigation-bar"
+    :class="['secondary-navigation-bar', classes]"
     :background="background"
     padding="medium"
   >
@@ -42,7 +48,37 @@ const background = computed(() =>
   display: grid;
   align-items: center;
   grid-template-columns: auto 1fr auto;
+  grid-template-rows: 1fr;
+  grid-template-areas: 'before title right';
   gap: var(--spacing-large);
+
+  .before {
+    grid-area: before;
+  }
+
+  .title {
+    grid-area: title;
+  }
+
+  .right {
+    grid-area: right;
+  }
+
+  &--centered {
+    justify-self: center;
+    .title {
+      grid-area: 1 / span 2;
+      justify-self: center;
+    }
+
+    .right {
+      display: none;
+    }
+
+    .before {
+      display: none;
+    }
+  }
 
   @media (max-width: 700px) {
     grid-template-columns: 1fr auto 1fr;
