f377b6f84bd90926e2b0ee3aaf37b654b39afc7f
Kryštof Korb <krystof@korb.cz>
2022-09-09 00:30:47 +0200

    Add Seznam Zprávy embed

---
+++ frontend/src/components/embeds/SeznamZpravyEmbed.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ThemeProvider from '@/components/ThemeProvider.vue';
+</script>
+
+<template>
+  <ThemeProvider theme="seznam-zpravy">
+    <div>Najdu tam, co neznám.</div>
+    <slot />
+  </ThemeProvider>
+</template>
