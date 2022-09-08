c528c15ef6e3b2c4ac6c93d02c501e678c924baa
Kryštof Korb <krystof@korb.cz>
2022-09-08 08:35:29 +0200

    Refactor guide to use routing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -114,8 +114,11 @@ export const appRoutes = {
   },
   guide: {
     name: 'guide',
-    path: '/kalkulacka/:election/:district/navod',
-    alias: '/kalkulacka/:election/:district',
+    path: '/kalkulacka/:election/:district/navod/:step?',
+    alias: [
+      '/kalkulacka/:election/:district',
+      '/kalkulacka/:election/:district/navod',
+    ],
     component: GuidePageVue,
     meta: {
       title: 'Návod - Volebni kalkulacka',
