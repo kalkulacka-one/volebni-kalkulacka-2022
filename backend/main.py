cf04edc69110aca9c66ab1623203885d8cff892f
Martin Majlis <martin@majlis.cz>
2022-09-08 10:10:53 +0200

    Include calculator into results

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from enum import Enum
 import os
+from typing import Optional
 import uuid
 
 import boto3
@@ -43,9 +45,35 @@ class Match(BaseModel):
     score: int
 
 
+class ElectionType(str, Enum):
+    senatni = "senatni"
+    prezidentske = "prezidentske"
+    snemovni = "snemovni"
+    krajske = "krajske"
+    municipalni = "municipalni"
+
+
+class Election(BaseModel):
+    id: str
+    key: str
+    name: str
+    description: str
+    type: ElectionType
+
+
+class Calculator(BaseModel):
+    id: str
+    name: str
+    description: str
+    district_code: str
+    election: Election
+
+
 class ResultIn(BaseModel):
     answers: list[Answer]  # type: ignore  # noqa
     matches: list[Match]  # type: ignore  # noqa
+    calculator: Calculator
+    source: Optional[str] = None
 
 
 class ResultOut(BaseModel):
@@ -53,6 +81,8 @@ class ResultOut(BaseModel):
     created_at: datetime
     answers: list[Answer]  # type: ignore  # noqa
     matches: list[Match]  # type: ignore  # noqa
+    calculator: Calculator
+    source: Optional[str] = None
 
 
 class ResultAdded(BaseModel):
@@ -78,12 +108,28 @@ def results_add(result: ResultIn):
 def results_get(result_id: str):
     response = get_table().get_item(Key={"result_id": result_id})
     item = response["Item"]
+    result = item["result"]
+    calculator = result["calculator"]
     return ResultOut(
         result_id=item["result_id"],
         created_at=datetime.fromisoformat(item["created_at"]),
-        answers=[Answer(**a) for a in item["result"]["answers"]],
+        answers=[Answer(**a) for a in result["answers"]],
         matches=[
             Match(candidate=Candidate(**m["candidate"]), score=m["score"])
-            for m in item["result"]["matches"]
+            for m in result["matches"]
         ],
+        calculator=Calculator(
+            id=calculator["id"],
+            name=calculator["name"],
+            description=calculator["description"],
+            district_code=calculator["district_code"],
+            election=Election(
+                id=calculator["election"]["id"],
+                key=calculator["election"]["key"],
+                name=calculator["election"]["name"],
+                description=calculator["election"]["description"],
+                type=ElectionType(calculator["election"]["type"]),
+            ),
+        ),
+        source=result.get("source"),
     )
