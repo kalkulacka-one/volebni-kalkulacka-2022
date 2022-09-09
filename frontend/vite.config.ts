2550b567af2a58f3952912b39ce96e4e60363317
Kryštof Korb <krystof@korb.cz>
2022-09-09 21:42:50 +0200

    Add vite-plugin-md

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Markdown from 'vite-plugin-md';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({ include: [/\.vue$/, /\.md$/] }), Markdown()],
   server: {
     host: '0.0.0.0',
     port: 5201,
