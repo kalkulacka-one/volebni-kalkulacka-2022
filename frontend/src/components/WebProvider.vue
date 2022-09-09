47d1556a9448394ad0ee64040052987128f5a927
Ondrej Kyjanek <ondrej.kyjanek@email.cz>
2022-09-09 10:32:02 +0200

    Add embed routing (#85)

---
+++ frontend/src/components/WebProvider.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ThemeProvider from '@/components/ThemeProvider.vue';
+</script>
+
+<template>
+  <ThemeProvider>
+    <router-view />
+  </ThemeProvider>
+</template>
