9135292798b436c0b354f52abfee4c48257f4a57
Kryštof Korb <krystof@korb.cz>
2022-09-10 10:43:44 +0200

    Add Markdown support (#94)
    * Add vite-plugin-md
    
    * Add Markdown component
    
    * Add types

---
+++ frontend/src/components/utilities/MarkdownIt.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import markdownIt from 'markdown-it';
+
+export interface Props {
+  markdown: string;
+}
+
+const props = defineProps<Props>();
+
+const renderer = new markdownIt();
+const html = computed(() => renderer.render(props.markdown));
+</script>
+
+<template>
+  <div v-html="html" />
+</template>
