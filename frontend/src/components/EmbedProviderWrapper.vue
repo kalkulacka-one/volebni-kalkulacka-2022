888344d8c71733250e42f7ff1499854e907d3891
Kryštof Korb <krystof@korb.cz>
2022-09-10 15:47:14 +0200

    Add embed wrapper (#105)

--- frontend/src/components/EmbedProviderWrapper.vue
+++ frontend/src/components/EmbedProviderWrapper.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, provide } from 'vue';
 import { useRoute } from 'vue-router';
 
 import EmbedProvider from '@/components/EmbedProvider.vue';
+import { EmbedKey } from '@/components/EmbedKey';
 
 const route = useRoute();
 
 const embed = computed(() => route.query.embed as string);
+
+provide(EmbedKey, embed.value);
 </script>
 
 <template>
