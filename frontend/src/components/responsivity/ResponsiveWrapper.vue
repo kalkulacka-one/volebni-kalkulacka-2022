78ae05e91b017cb1b5fa6a50493df5c8864ea387
Kryštof Korb <krystof@korb.cz>
2022-09-10 14:25:42 +0200

    Add responsive wrapper

---
+++ frontend/src/components/responsivity/ResponsiveWrapper.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+export interface Props {
+  desktop?: boolean;
+  mobile?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  desktop: true,
+  mobile: true,
+});
+
+const classes = computed(() => ({
+  'responsive-wrapper--desktop': props.desktop,
+  'responsive-wrapper--mobile': props.mobile,
+}));
+</script>
+
+<template>
+  <div :class="['responsive-wrapper', classes]">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.responsive-wrapper {
+  display: none;
+
+  &--desktop {
+    @media (min-width: 601px) {
+      display: initial;
+    }
+  }
+
+  &--mobile {
+    @media (max-width: 600px) {
+      display: initial;
+    }
+  }
+}
+</style>
