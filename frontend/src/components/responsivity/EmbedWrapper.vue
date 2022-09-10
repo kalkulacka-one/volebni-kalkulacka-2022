4e37104b5ba2fd56198b4c759a2a023f2eb3d018
Kryštof Korb <krystof@korb.cz>
2022-09-10 15:29:09 +0200

    Add embed wrapper

---
+++ frontend/src/components/responsivity/EmbedWrapper.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed, inject } from 'vue';
+
+import { EmbedKey } from '@/components/EmbedKey';
+
+export interface Props {
+  embed?: boolean | string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  embed: false,
+});
+
+const currentEmbed = inject(EmbedKey);
+
+const classes = computed(() => ({
+  [`embed-wrapper--${currentEmbed}`]: props.embed,
+}));
+</script>
+
+<template>
+  <div
+    v-if="embed === true || embed === currentEmbed"
+    :class="['embed-wrapper', classes]"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.embed-wrapper {
+  display: contents;
+}
+</style>
