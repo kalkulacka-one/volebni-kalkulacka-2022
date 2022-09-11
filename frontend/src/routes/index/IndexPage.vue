240e43e51f53cc2847ae21b96205488521ffd742
Martin Wenisch <martin.wenisch@cesko.digital>
2022-09-11 19:02:40 +0200

    Linter - escapes fix

--- frontend/src/routes/index/IndexPage.vue
+++ frontend/src/routes/index/IndexPage.vue
@@ -40,10 +40,10 @@ export default {
           w["DarujmeObject"] = u;
           w[u] = w[u] || function () { (w[u].q = w[u].q || []).push(arguments) };
           a = d.createElement(s); b = d.getElementsByTagName(s)[0];
-          a.async = 1; a.src = "https:\/\/www.darujme.cz\/assets\/scripts\/widget.js";
+          a.async = 1; a.src = "https://www.darujme.cz/assets/scripts/widget.js";
           b.parentNode.insertBefore(a, b);
         }(window, document, "script", "Darujme");
-    Darujme(1, "w2acrk0w61fgr3so", 'render', "https:\/\/www.darujme.cz\/widget?token=w2acrk0w61fgr3so", "100%");`);
+    Darujme(1, "w2acrk0w61fgr3so", 'render', "https://www.darujme.cz/widget?token=w2acrk0w61fgr3so", "100%");`);
   },
 };
 </script>
