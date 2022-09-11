5c80fae9523980e430c5285a018d69dbd539ff78
Martin Majlis <martin@majlis.cz>
2022-09-11 13:27:12 +0200

    Log more information about JSON files when validating

--- schemas/validate.sh
+++ schemas/validate.sh
@@ -4,11 +4,14 @@ echo "Validate calculators";
 for f in `find ../data/kalkulacka/ -mindepth 2 -name '*.json'`; do
     echo "Validating ${f}";
     ajv validate --verbose -s calculator.schema.json -d "${f}" -r candidate-answer.schema.json -r candidate.schema.json -r contact.schema.json -r election.schema.json -r party.schema.json -r question.schema.json;
-    echo ""
     echo "Candidates: "`jq '.candidates | length' "${f}"`;
     echo "Questions: "`jq '.questions | length' "${f}"`;
     echo "Answers: "`jq '.answers | length' "${f}"`;
+    echo ""
 done;
 
 echo "Validate list of calculators"
-ajv validate --verbose -s calculators.schema.json -d ../data/kalkulacka/calculators.json -r election.schema.json
+calculators="../data/kalkulacka/calculators.json";
+ajv validate --verbose -s calculators.schema.json -d "${calculators}" -r election.schema.json
+echo "Elections: "`jq '.elections | length' "${calculators}"`;
+echo "Calculators: "`jq '.calculators | length' "${calculators}"`;
