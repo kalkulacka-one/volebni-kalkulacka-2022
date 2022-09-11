98d9df7da740d2221e5b03d7251a6dafd5a4087a
Kryštof Korb <krystof@korb.cz>
2022-09-11 09:01:58 +0200

    Refactor bottom bar

--- frontend/src/components/design-system/navigation/BottomBar.vue
+++ frontend/src/components/design-system/navigation/BottomBar.vue
@@ -4,22 +4,15 @@ import { computed } from 'vue';
 import ContainerComponent from '@/components/design-system/containers/ContainerComponent.vue';
 
 export interface Props {
-  desktop?: boolean;
-  mobile?: boolean;
-  transparent: 'always' | 'never' | 'desktop' | 'mobile';
+  transparent?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  desktop: true,
-  mobile: true,
+  transparent: false,
 });
 
 const classes = computed(() => ({
-  'bottom-bar--transparent': props.transparent === 'always',
-  'bottom-bar--transparent-desktop': props.transparent === 'desktop',
-  'bottom-bar--transparent-mobile': props.transparent === 'mobile',
-  'bottom-bar--desktop': props.desktop,
-  'bottom-bar--mobile': props.mobile,
+  'bottom-bar--transparent': props.transparent,
 }));
 </script>
 
@@ -31,34 +24,8 @@ const classes = computed(() => ({
 
 <style scoped lang="scss">
 .bottom-bar {
-  display: none;
-
-  &--desktop {
-    @media (min-width: 700px) {
-      display: block;
-    }
-  }
-
-  &--mobile {
-    @media (max-width: 700px) {
-      display: block;
-    }
-  }
-
   &--transparent {
     background-color: transparent;
-  }
-
-  &--transparent-desktop {
-    @media (min-width: 700px) {
-      background-color: transparent;
-    }
-  }
-
-  &--transparent-mobile {
-    @media (max-width: 700px) {
-      background-color: transparent;
-    }
   }
 }
 </style>
