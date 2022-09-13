b33a0a2d96338f23ee35b30330ac745def36a39f
Kryštof Korb <krystof@korb.cz>
2022-09-13 19:32:23 +0200

    Refactor divider

--- frontend/src/components/design-system/containers/DividerComponent.vue
+++ frontend/src/components/design-system/containers/DividerComponent.vue
@@ -1,14 +1,72 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+export interface Props {
+  weight?: 'light' | 'normal' | 'bold';
+  dotted?: boolean;
+  vertical?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  weight: 'normal',
+  dotted: false,
+  vertical: false,
+});
+
+const classes = computed(() => ({
+  [`divider--weight-${props.weight}`]: props.weight,
+  'divider--dotted': props.dotted,
+  'divider--vertical': props.vertical,
+}));
+
+const tag = computed(() => (props.vertical ? 'div' : 'hr'));
+</script>
+
 <template>
-  <hr class="divider" />
+  <component :is="tag" :class="['divider', classes]" />
 </template>
 
 <style lang="scss" scoped>
 .divider {
-  background-color: rgb(var(--color-neutral-border));
-  margin: var(--spacing-small);
-  border: 0;
-  border-top-width: var(--spacing-pico);
-  border-top-style: solid;
-  border-top-color: rgb(var(--color-neutral-border));
+  margin: 0;
+  height: 0;
+  border-style: solid;
+  border-color: rgb(var(--color-neutral-border));
+
+  &--weight {
+    &-light {
+      border-width: var(--spacing-pico);
+      border-radius: var(--spacing-pico);
+    }
+
+    &-normal {
+      border-width: var(--spacing-nano);
+      border-radius: var(--spacing-nano);
+    }
+
+    &-bold {
+      border-width: var(--spacing-tiny);
+      border-radius: var(--spacing-tiny);
+    }
+  }
+
+  &--dotted {
+    border-style: dotted;
+  }
+
+  &--vertical {
+    width: 0;
+    height: 100%;
+    border-top-width: 0 !important;
+    border-bottom-width: 0 !important;
+    border-left-width: 0 !important;
+    border-radius: initial;
+  }
+
+  &:not(.divider--vertical) {
+    border-right-width: 0 !important;
+    border-bottom-width: 0 !important;
+    border-left-width: 0 !important;
+  }
 }
 </style>
