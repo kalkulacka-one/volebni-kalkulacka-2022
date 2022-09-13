c79988d7163651af490ca4562ba9b18e752fe1b7
Kryštof Korb <krystof@korb.cz>
2022-09-14 00:02:34 +0200

    Refactor divider (#175)

--- frontend/src/components/design-system/containers/DividerComponent.vue
+++ frontend/src/components/design-system/containers/DividerComponent.vue
@@ -1,14 +1,69 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+export interface Props {
+  weight?: 'light' | 'normal' | 'bold';
+  lineStyle?: 'solid' | 'dashed' | 'dotted';
+  color?: string;
+  vertical?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  weight: 'normal',
+  lineStyle: 'solid',
+  color: 'rgb(var(--color-neutral-border))',
+  vertical: false,
+});
+
+const classes = computed(() => ({
+  [`divider--weight-${props.weight}`]: props.weight,
+  'divider--vertical': props.vertical,
+}));
+
+const tag = computed(() => (props.vertical ? 'div' : 'hr'));
+</script>
+
 <template>
-  <hr class="divider" />
+  <component :is="tag" :class="['divider', classes]" />
 </template>
 
 <style lang="scss" scoped>
 .divider {
-  background-color: rgb(var(--color-neutral-border));
-  margin: var(--spacing-small);
-  border: 0;
-  border-top-width: var(--spacing-pico);
-  border-top-style: solid;
-  border-top-color: rgb(var(--color-neutral-border));
+  margin: 0;
+  height: 0;
+  border-style: v-bind(lineStyle);
+  border-color: v-bind(color);
+
+  &--weight {
+    &-light {
+      border-width: var(--spacing-pico);
+      border-radius: var(--spacing-pico);
+    }
+
+    &-normal {
+      border-width: var(--spacing-nano);
+      border-radius: var(--spacing-nano);
+    }
+
+    &-bold {
+      border-width: var(--spacing-tiny);
+      border-radius: var(--spacing-tiny);
+    }
+  }
+
+  &--vertical {
+    width: 0;
+    height: 100%;
+    border-top-width: 0 !important;
+    border-bottom-width: 0 !important;
+    border-left-width: 0 !important;
+    border-radius: initial;
+  }
+
+  &:not(.divider--vertical) {
+    border-right-width: 0 !important;
+    border-bottom-width: 0 !important;
+    border-left-width: 0 !important;
+  }
 }
 </style>
