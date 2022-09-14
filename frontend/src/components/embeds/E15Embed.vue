2949877654744593315ddb3811d5959f7430da63
Michal Skop <michal.skop@kohovolit.eu>
2022-09-14 18:54:34 +0200

    E15 embed + color scheme

---
+++ frontend/src/components/embeds/E15Embed.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ThemeProvider from '@/components/ThemeProvider.vue';
+</script>
+
+<template>
+  <ThemeProvider theme="blesk">
+    <slot />
+  </ThemeProvider>
+</template>
