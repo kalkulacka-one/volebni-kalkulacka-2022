bb2e43077f931d9d2ed0a9ec3e3ee3997d7030da
Michal Škop <michal.skop@seznam.cz>
2022-09-14 19:39:22 +0200

    E15 embed + color scheme (#183)
    * E15 embed + color scheme
    
    * Update frontend/src/components/embeds/E15Embed.vue
    
    Co-authored-by: Kryštof Korb <krystof@korb.cz>
    
    Co-authored-by: Kryštof Korb <krystof@korb.cz>

---
+++ frontend/src/components/embeds/E15Embed.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ThemeProvider from '@/components/ThemeProvider.vue';
+</script>
+
+<template>
+  <ThemeProvider theme="e15">
+    <slot />
+  </ThemeProvider>
+</template>
