a7826188304077c0605ea8ae0ed389d35ec08a53
Martin Wenisch <martin.wenisch@cesko.digital>
2022-11-18 17:20:04 +0100

    prettier

--- frontend/api/test.js
+++ frontend/api/test.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-    const { name = 'World' } = req.query;
-    return res.send(`Hello ${name}!`);
-  }
+  const { name = 'World' } = req.query;
+  return res.send(`Hello ${name}!`);
+}
