f51997ebabc231c4ed4a6e3154adc71d21129fa4
Kryštof Korb <krystof@korb.cz>
2022-11-26 13:57:26 +0100

    Add masonry grid component draft

---
+++ frontend/src/components/design-system/layout/MasonryGrid.vue
@@ -0,0 +1,13 @@
+<template>
+  <div :class="['masonry-grid']">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.masonry-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
+  gap: var(--responsive-spacing-medium);
+}
+</style>
