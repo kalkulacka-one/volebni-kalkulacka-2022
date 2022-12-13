b7901984382686ef6e2a3e73486069d317d8f2c3
Tibor Kulčár <kulcar.tibor@gmail.com>
2022-12-13 16:30:54 +0100

    introduce TimelineComponent

---
+++ frontend/src/components/design-system/containers/TimelineItemComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <li class="timeline-item">
+    <slot />
+  </li>
+</template>
+
+<style scoped lang="scss">
+.timeline-item {
+  position: relative;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    background-color: rgb(var(--palette-neutral-30));
+  }
+
+  &:before {
+    top: var(--base-medium);
+    left: calc(var(--base-small) * -1);
+    width: var(--base-smaller);
+    height: var(--base-smaller);
+    border-radius: 50%;
+  }
+
+  &:after {
+    top: var(--spacing-huge);
+    left: calc(0.65rem * -1);
+    height: calc(100% - var(--base-smaller));
+    width: 1px;
+  }
+}
+</style>
