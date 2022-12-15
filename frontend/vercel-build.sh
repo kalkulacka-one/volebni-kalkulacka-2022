3a3c5f8082b98b3fcb873353fd48449510ded194
Kryštof Korb <krystof@korb.cz>
2022-12-15 17:29:23 +0100

    Format Vercel script

--- frontend/vercel-build.sh
+++ frontend/vercel-build.sh
@@ -4,9 +4,9 @@ echo $VERCEL_GIT_COMMIT_REF
 echo $VERCEL_ENV
 
 if [[ $VERCEL_ENV == "production" ]]; then
-    prisma generate && prisma migrate deploy && npm run build && cp -r ../data ./dist/
+  prisma generate && prisma migrate deploy && npm run build && cp -r ../data ./dist/
 else
-export DATABASE_URL="$DATABASE_URL_BASE/$VERCEL_GIT_COMMIT_REF"
+  export DATABASE_URL="$DATABASE_URL_BASE/$VERCEL_GIT_COMMIT_REF"
 
-prisma generate && prisma migrate reset --force && npm run build && cp -r ../data ./dist/
+  prisma generate && prisma migrate reset --force && npm run build && cp -r ../data ./dist/
 fi
