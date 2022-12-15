8f862b6c7834d0510a5e15153325165374cb40b4
Martin Wenisch <martin.wenisch@cesko.digital>
2022-12-15 17:48:55 +0100

    Update frontend/vercel-build.sh
    Co-authored-by: Kryštof Korb <krystof@korb.cz>

--- frontend/vercel-build.sh
+++ frontend/vercel-build.sh
@@ -2,6 +2,7 @@
 
 
 if [[ $VERCEL_ENV == "production" ]]; then
+  echo "Running production database migration"
   prisma generate && prisma migrate deploy && npm run build && cp -r ../data ./dist/
 else
   export DATABASE_URL="$DATABASE_URL_BASE/$VERCEL_GIT_COMMIT_REF"
