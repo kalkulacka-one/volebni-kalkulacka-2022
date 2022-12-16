8dfc705e01b94399a9ad4d1b31981b40a892c93e
Kryštof Korb <krystof@korb.cz>
2022-12-16 16:18:24 +0100

    Use build-only for Vercel

--- frontend/vercel-build.sh
+++ frontend/vercel-build.sh
@@ -2,7 +2,7 @@
 
 if [[ $VERCEL_ENV == "production" ]]; then
   echo "Building for production environment, running database migration"
-  prisma generate && prisma migrate deploy && npm run build && cp -r ../data ./dist/
+  prisma generate && prisma migrate deploy && npm run build-only && cp -r ../data ./dist/
 else
   echo "Building for \`$VERCEL_ENV\` environment, using a clean database for \`$VERCEL_GIT_COMMIT_REF\` branch"
   export DATABASE_URL="$DATABASE_URL_BASE/$VERCEL_GIT_COMMIT_REF"
