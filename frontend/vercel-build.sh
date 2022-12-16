6a57ee901ef6cbee9704ae4328bbd40795afc11f
Kryštof Korb <krystof@korb.cz>
2022-12-16 15:00:47 +0100

    Fix Vercel build script

--- frontend/vercel-build.sh
+++ frontend/vercel-build.sh
@@ -4,7 +4,7 @@ if [[ $VERCEL_ENV == "production" ]]; then
   echo "Running production database migration"
   prisma generate && prisma migrate deploy && npm run build && cp -r ../data ./dist/
 else
-  echo "Running on $($VERCEL_ENV), using a clean database for $($VERCEL_GIT_COMMIT_REF) commit"
+  echo "Running on $VERCEL_ENV, using a clean database for $VERCEL_GIT_COMMIT_REF commit"
   export DATABASE_URL="$DATABASE_URL_BASE/$VERCEL_GIT_COMMIT_REF"
   prisma generate && prisma migrate reset --force && npm run build && cp -r ../data ./dist/
 fi
