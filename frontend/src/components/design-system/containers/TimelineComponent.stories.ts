ecbdcacb2d8f7025cf5df3b4d64f540a68797a19
Tibor Kulčár <kulcar.tibor@gmail.com>
2022-12-21 16:58:09 +0100

    Introduce TimelineComponent (#301)

---
+++ frontend/src/components/design-system/containers/TimelineComponent.stories.ts
@@ -0,0 +1,45 @@
+import BodyText from '@/components/design-system/typography/BodyText.vue';
+import ButtonComponent from '@/components/design-system/input/ButtonComponent.vue';
+import CardComponent from './CardComponent.vue';
+import TitleText from '@/components/design-system/typography/TitleText.vue';
+import TimelineComponent from './TimelineComponent.vue';
+import TimelineItemComponent from './TimelineItemComponent.vue';
+
+import type { Meta, StoryFn } from '@storybook/vue3';
+
+export default {
+  title: 'Containers/TimelineComponent',
+  component: TimelineComponent,
+} as Meta<typeof TimelineComponent>;
+
+export const WithCards: StoryFn<typeof TimelineComponent> = () => ({
+  components: {
+    BodyText,
+    ButtonComponent,
+    CardComponent,
+    TitleText,
+    TimelineComponent,
+    TimelineItemComponent,
+  },
+  template: `
+    <TimelineComponent>
+      <TimelineItemComponent v-for="a in 6">
+        <CardComponent corner="top-right" padding="medium" border shadow>
+          <div class="card-content">
+            <div class="card-content-text">
+              <TitleText tag="h3" size="medium">Komunální volby 2022</TitleText>
+              <BodyText size="small">Komunální kalkulačku vyplnilo přes 100 tisíc lidí.</BodyText>
+            </div>
+          </div>
+        </CardComponent>
+      </TimelineItemComponent>
+      <TimelineItemComponent>
+        <CardComponent corner="top-right" padding="medium" border shadow>
+          <div class="card-content">
+            <TitleText size="extra-small" tag="h5">Tohle je poslední položka</TitleText>
+          </div>
+        </CardComponent>
+      </TimelineItemComponent>
+    </TimelineComponent>
+  `,
+});
---
+++ frontend/src/components/design-system/containers/TimelineComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ul class="timeline">
+    <slot />
+  </ul>
+</template>
+
+<style scoped lang="scss">
+.timeline {
+  display: flex;
+  flex-direction: column;
+  gap: var(--base-medium);
+  padding-left: var(--base-small);
+  margin: 0;
+  list-style-type: none;
+}
+</style>
