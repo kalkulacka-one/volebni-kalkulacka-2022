c2c605574098c90e8d4f148455ee8d4f78b5d54c
Ondrej Kyjanek <ondrej.kyjanek@email.cz>
2022-12-22 13:43:31 +0100

    fixed routing and initialization of store

--- frontend/src/routes/error/ErrorPage.vue
+++ frontend/src/routes/error/ErrorPage.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { appRoutes } from '@/main';
+import { computed } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 
 export interface Props {
-  case: 'api-error-election' | 'api-error-district';
+  case: 'not-found' | 'api-error-election' | 'api-error-calculator';
 }
 
 const props = defineProps<Props>();
@@ -15,24 +16,35 @@ const handleHomeClicked = () => {
     query: { ...route.query },
   });
 };
-let header = 'Unknown Error';
-let details = 'Unknown error occurred!';
-switch (props.case) {
-  case 'api-error-election':
-    header = 'Api Error';
-    details = 'Could not load election information.';
-    break;
-  case 'api-error-district':
-    header = 'Api Error';
-    details = 'Could not load district information.';
-    break;
-  default:
-    break;
-}
+const pageInfo = computed(() => {
+  let header = 'Unknown Error';
+  let details = 'Unknown error occurred!';
+  switch (props.case) {
+    case 'not-found':
+      header = 'Not Found';
+      details = 'The requested page does not exist!';
+      break;
+    case 'api-error-election':
+      header = 'Api Error';
+      details = 'Could not load election information.';
+      break;
+    case 'api-error-calculator':
+      header = 'Api Error';
+      details = 'Could not load district information.';
+      break;
+    default:
+      break;
+  }
+  console.debug(props.case);
+  return {
+    header: header,
+    details: details,
+  };
+});
 </script>
 <template>
   <h1>Error</h1>
-  <h3>{{ header }}</h3>
-  <p>{{ details }}</p>
+  <h3>{{ pageInfo.header }}</h3>
+  <p>{{ pageInfo.details }}</p>
   <button @click="handleHomeClicked">Domu</button>
 </template>
