bd9f3bccd7a25ddac139d0303e2f4778e5334f5a
Pavel Vojacek <pavel.vojacek@fragaria.cz>
2022-12-22 11:25:13 +0100

    Verify user when result is created

--- frontend/api/answers/index.ts
+++ frontend/api/answers/index.ts
@@ -1,13 +1,29 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Prisma } from '@prisma/client';
 import { prisma } from '../../src/server/prisma';
+import { authUser } from '../../src/server/auth';
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
     const value = req.body.value as Prisma.JsonObject;
+    const userId = req.body.userId;
+
+    if (userId) {
+      // If userId is provided, check that the user is authorized and matches the userId
+      try {
+        const user = await authUser(req);
+        if (user.id !== userId) throw new Error('Unauthorized');
+      } catch (err) {
+        return res
+          .status(401)
+          .send({ error: 'Unauthorized', msg: err.message });
+      }
+    }
+
     const result = await prisma.result.create({
       data: {
         value: value,
+        userId: userId,
         source: req.body.source,
         embedSourceUrl: req.body.embedSourceUrl,
       },
