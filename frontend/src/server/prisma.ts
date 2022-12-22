50713794f85019ad6ef96fd0f2cc5604ff0799a3
Kryštof Korb <krystof@korb.cz>
2022-12-22 13:12:19 +0100

    Use commit SHA instead of branch name for database name

--- frontend/src/server/prisma.ts
+++ frontend/src/server/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const connectionUrl =
   process.env.VERCEL_ENV === 'preview'
-    ? `${process.env.DATABASE_URL_BASE}/${process.env.VERCEL_GIT_COMMIT_REF}`
+    ? `${process.env.DATABASE_URL_BASE}/${process.env.VERCEL_GIT_COMMIT_SHA}`
     : process.env.DATABASE_URL;
 
 export const prisma = new PrismaClient({
