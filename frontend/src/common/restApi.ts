65d69f8d4d8217a5a8c455296bdadc2105301ce6
Kryštof Korb <krystof@korb.cz>
2022-12-28 19:33:28 +0100

    Save important only if set (#381)

--- frontend/src/common/restApi.ts
+++ frontend/src/common/restApi.ts
@@ -34,9 +34,11 @@ const buildResultData = () => {
     .map((x) => {
       if (x.answer == UserAnswerEnum.yes || x.answer == UserAnswerEnum.no) {
         return {
-          question_id: x.id,
-          answer: convertAnswerToBool(x.answer),
-          is_important: x.flag,
+          ...{
+            questionId: x.id,
+            answer: convertAnswerToBool(x.answer),
+          },
+          ...(x.flag ? { isImportant: x.flag } : {}),
         };
       }
     })
@@ -53,7 +55,7 @@ const buildResultData = () => {
       throw new Error(`Unknown candidate ${x.cId}`);
     }
     return {
-      candidate_id: candidate.id,
+      candidateId: candidate.id,
       score: x.result.result_percent,
     };
   });
@@ -102,8 +104,8 @@ export const getResults = async (resultId: string) => {
     resParsed.answers as ReturnType<typeof buildResultData>['answers']
   ).map((x) => {
     return {
-      id: x.question_id,
-      flag: x.is_important,
+      id: x.questionId,
+      flag: x.isImportant || false,
       answer: convertBoolToAnswer(x.answer),
     };
   });
