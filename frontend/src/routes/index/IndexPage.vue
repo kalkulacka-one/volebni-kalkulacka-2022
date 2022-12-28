2fdd2e1187b7c86607861cd2547d2432b8f64fd9
Kryštof Korb <krystof@korb.cz>
2022-12-28 23:54:46 +0100

    Add link to Názory politiků

--- frontend/src/routes/index/IndexPage.vue
+++ frontend/src/routes/index/IndexPage.vue
@@ -189,7 +189,7 @@ const scrollDown = () => info.value?.scrollIntoView({ behavior: 'smooth' });
             </BodyText>
             <BodyText size="small">
               Otázky, které rezonují mladou generací.<br />
-              Ve spolupráci s projektem Názory politiků.
+              Ve spolupráci s projektem <a href="https://www.nazorypolitiku.cz" target="_blank">Názory politiků</a>.
             </BodyText>
             <ButtonComponent
               kind="outlined"
