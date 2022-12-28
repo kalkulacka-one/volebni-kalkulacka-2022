a2b21641eec009fd9d675fc5fea0f559b6bfa3dc
Kryštof Korb <krystof@korb.cz>
2022-12-28 23:56:52 +0100

    Run Prettier

--- frontend/src/routes/index/IndexPage.vue
+++ frontend/src/routes/index/IndexPage.vue
@@ -189,7 +189,10 @@ const scrollDown = () => info.value?.scrollIntoView({ behavior: 'smooth' });
             </BodyText>
             <BodyText size="small">
               Otázky, které rezonují mladou generací.<br />
-              Ve spolupráci s projektem <a href="https://www.nazorypolitiku.cz" target="_blank">Názory politiků</a>.
+              Ve spolupráci s projektem
+              <a href="https://www.nazorypolitiku.cz" target="_blank">
+                Názory politiků
+              </a>.
             </BodyText>
             <ButtonComponent
               kind="outlined"
