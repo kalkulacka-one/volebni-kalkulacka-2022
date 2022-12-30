eece124c8cc5c438fa7161eb1f4a5185ebc01315
Michal Škop <michal.skop@seznam.cz>
2022-12-30 12:20:56 +0100

    Add ČT24 and A2larm embed (#402)

---
+++ frontend/src/components/themes/A2larmTheme.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped src="@/components/themes/A2larmTheme.scss" />
---
+++ frontend/src/components/themes/Ct24Theme.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped src="@/components/themes/Ct24Theme.scss" />
