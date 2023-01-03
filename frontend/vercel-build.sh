19301564715ea989196a8384dfe38b1dc5605ab0
Kryštof Korb <krystof@korb.cz>
2023-01-03 20:44:25 +0100

    Update Vercel build script

--- frontend/vercel-build.sh
+++ frontend/vercel-build.sh
@@ -3,9 +3,13 @@
 if [[ $VERCEL_ENV == "production" ]]; then
   echo "Building for production environment, running database migration"
   prisma generate && prisma migrate deploy && npm run build-only && cp -r ../data ./dist/
+elif [[ $VERCEL_GIT_COMMIT_REF == "staging" ]]; then
+  echo "Building for preview environment, running database migration for \`staging\` branch"
+  export DATABASE_URL="$DATABASE_URL_BASE/staging"
+  prisma generate && prisma migrate deploy && npm run build-only:preview && cp -r ../data ./dist/
 else
-  echo "Building for \`$VERCEL_ENV\` environment, using a clean database for \`$VERCEL_GIT_COMMIT_REF\` branch"
+  echo "Building for preview environment, using a clean database for \`$VERCEL_GIT_COMMIT_REF\` branch"
   GIT_COMMIT_REF_SHA=$(echo -n $VERCEL_GIT_COMMIT_REF | openssl dgst -sha256 | sed 's/^.* //')
   export DATABASE_URL="$DATABASE_URL_BASE/$GIT_COMMIT_REF_SHA"
-  prisma generate && prisma migrate reset --force && npm run build-only -- --mode $VERCEL_ENV && cp -r ../data ./dist/
+  prisma generate && prisma migrate reset --force && npm run build-only:preview && cp -r ../data ./dist/
 fi
