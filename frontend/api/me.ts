22cedcdcf4205580f382046c18e021f040e96e87
Pavel Vojacek <pavel.vojacek@fragaria.cz>
2023-01-03 13:47:06 +0100

    Add /api/me DELETE method

--- frontend/api/me.ts
+++ frontend/api/me.ts
@@ -1,15 +1,36 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { prisma } from '../src/server/prisma';
 import { authUser } from '../src/server/auth';
-import { respond401 } from '../src/server/errors';
+import {
+  respond401,
+  respond405,
+  prismaErrorHandler,
+} from '../src/server/errors';
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  try {
+  if (req.method === 'GET') {
+    try {
+      const user = await authUser(req, res);
+      if (user) {
+        return res.send({ user: user });
+      }
+      return respond401(res, 'No auth cookie.');
+    } catch (err) {
+      return respond401(res);
+    }
+  } else if (req.method === 'DELETE') {
     const user = await authUser(req, res);
     if (user) {
-      return res.send({ user: user });
+      await prisma.$transaction([
+        prisma.answers.updateMany({
+          where: { userId: user.id },
+          data: { userId: null },
+        }),
+        prisma.user.delete({ where: { id: user.id } }),
+      ]);
+      return res.status(204).send(null);
     }
     return respond401(res, 'No auth cookie.');
-  } catch (err) {
-    return respond401(res);
   }
+  return respond405(res, 'Method not allowed.');
 }
