48925c581b2ac190486466082cdb16dec034a1c2
Pavel Vojacek <pavel.vojacek@fragaria.cz>
2023-01-03 15:41:03 +0100

    Refactor /api/me

--- frontend/api/me.ts
+++ frontend/api/me.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { prisma } from '../src/server/prisma';
+import { User } from '@prisma/client';
 import { authUser } from '../src/server/auth';
 import {
   respond401,
@@ -8,29 +9,31 @@ import {
 } from '../src/server/errors';
 
 export default async function (req: VercelRequest, res: VercelResponse) {
+  let user: void | User | null;
+  try {
+    user = await authUser(req, res);
+  } catch (err) {
+    return respond401(res);
+  }
+
+  if (!user) {
+    return respond401(res, 'User not found.');
+  }
+
   if (req.method === 'GET') {
-    try {
-      const user = await authUser(req, res);
-      if (user) {
-        return res.send({ user: user });
-      }
-      return respond401(res, 'No auth cookie.');
-    } catch (err) {
-      return respond401(res);
-    }
+    return res.send({ user: user });
   } else if (req.method === 'DELETE') {
-    const user = await authUser(req, res);
-    if (user) {
-      await prisma.$transaction([
+    await prisma
+      .$transaction([
         prisma.answers.updateMany({
           where: { userId: user.id },
           data: { userId: null },
         }),
         prisma.user.delete({ where: { id: user.id } }),
-      ]);
-      return res.status(204).send(null);
-    }
-    return respond401(res, 'No auth cookie.');
+      ])
+      .catch(prismaErrorHandler(res));
+    return res.status(204).send(null);
   }
-  return respond405(res, 'Method not allowed.');
+
+  respond405(res, req.method);
 }
