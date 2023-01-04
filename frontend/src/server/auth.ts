2a7328e516cb0323813d4231cd962079c00ae01c
Pavel Vojacek <pavel.vojacek@fragaria.cz>
2023-01-04 16:45:01 +0100

    Check ISS in jwt token

--- frontend/src/server/auth.ts
+++ frontend/src/server/auth.ts
@@ -3,12 +3,14 @@ import { verify } from 'jsonwebtoken';
 import { prisma } from './prisma';
 import { prismaErrorHandler } from './errors';
 import type { User } from '@prisma/client';
+import { getPublicUrl } from './utils';
 
 export async function authUser(
   req: VercelRequest,
   res: VercelResponse
 ): Promise<{ user: User; exp: number } | null> {
   const tokenString = req.cookies.auth;
+  const PUBLIC_URL = getPublicUrl();
   if (!tokenString) {
     return null;
   }
@@ -16,10 +18,14 @@ export async function authUser(
     const parsed = JSON.parse(tokenString);
     const token = parsed.token;
     const secret = process.env.JWT_SECRET as string;
-    const { sub, exp } = verify(token as string, secret) as {
+    const { sub, exp, iss } = verify(token as string, secret) as {
       sub: string;
       exp: number;
+      iss: string;
     };
+    if (iss !== PUBLIC_URL) {
+      return null;
+    }
     const user = await prisma.user.findUniqueOrThrow({
       where: { id: sub },
     });
