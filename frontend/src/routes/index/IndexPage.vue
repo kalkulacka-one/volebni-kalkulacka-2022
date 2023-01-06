dcc299806e850fd90e2073775c0dd513a11bee1b
Kryštof Korb <krystof@korb.cz>
2023-01-06 10:44:40 +0100

    Update outdated number

--- frontend/src/routes/index/IndexPage.vue
+++ frontend/src/routes/index/IndexPage.vue
@@ -78,7 +78,7 @@ const scrollDown = () => info.value?.scrollIntoView({ behavior: 'smooth' });
           </a>
         </BodyText>
         <BodyText size="small">
-          Volební kalkulačku už vyplnilo přes 250 tisíc lidí.
+          Volební kalkulačku už vyplnilo přes 500 tisíc lidí.
         </BodyText>
       </StackComponent>
       <div class="avatars">
