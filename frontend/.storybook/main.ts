6ae58da704ef0e1aee880e41371669969ab7deb9
Tibor Kulčár <kulcar.tibor@gmail.com>
2023-02-04 16:42:26 +0100

    upgrade to lastest possible vesion of storybook, exclude md files from storybook build

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,19 +1,29 @@
 import type { StorybookConfig } from '@storybook/types';
+
+const glob = require('glob');
+const path = require('path');
+const appDirectory = path.resolve(__dirname, '../');
+const getStories = () =>
+  glob.sync(`${appDirectory}/src/components/**/*.stories.@(js|jsx|ts|tsx)`, {
+    ignore: `${appDirectory}/src/routes/**/*.*)`,
+  });
+
 const config: StorybookConfig = {
-  stories: [
-    '../src/components/**/*.mdx',
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: async () => [...getStories()],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false,
+        docs: false,
+        backgrounds: false,
+      },
+    },
   ],
   framework: {
     name: '@storybook/vue3-vite',
-  },
-  docs: {
-    autodocs: false,
   },
 };
 module.exports = config;
