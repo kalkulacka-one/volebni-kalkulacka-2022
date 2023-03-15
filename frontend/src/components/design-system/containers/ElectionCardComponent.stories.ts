1c193892deda62d11d42a126544bcebb479afbca
Tibor Kulčár <kulcar.tibor@gmail.com>
2023-03-15 13:59:12 +0100

    fix ElectionCardComponent story

--- frontend/src/components/design-system/containers/ElectionCardComponent.stories.ts
+++ frontend/src/components/design-system/containers/ElectionCardComponent.stories.ts
@@ -7,6 +7,8 @@ export default {
   component: ElectionCardComponent,
 } as Meta<typeof ElectionCardComponent>;
 
+const imageUrl = 'https://loremflickr.com/320/320/face,human,person,profile';
+
 const Template: StoryFn<typeof ElectionCardComponent> = (args) => ({
   components: { ElectionCardComponent },
   setup() {
@@ -19,29 +21,29 @@ const Template: StoryFn<typeof ElectionCardComponent> = (args) => ({
 export const NotFilledIn = Template.bind({});
 NotFilledIn.args = {
   electionName: 'Prezidentská volba',
-  electionDateFrom: '3.',
-  electionDateTo: '4. ledna 2023',
+  electionDateFrom: '2023-01-13T14:00:00',
+  electionDateTo: '2023-01-17T14:00:00',
 };
 
 export const FilledIn = Template.bind({});
 FilledIn.args = {
   electionName: 'Senátní volby',
-  electionDateFrom: '23.',
-  electionDateTo: '24. září 2022',
+  electionDateFrom: '2023-03-13T14:00:00',
+  electionDateTo: '2023-04-17T14:00:00',
   candidates: [
     {
       id: 'a',
       name: 'Miluše Politická',
       description: 'Koalice pro práva občanů',
       score: 95,
-      image: 'https://thispersondoesnotexist.com/image',
+      image: imageUrl,
     },
     {
       id: 'b',
       name: 'Petr Apolitický',
       description: 'Koalice pro práva občanů',
       score: 93,
-      image: 'https://thispersondoesnotexist.com/image',
+      image: imageUrl,
     },
   ],
   updated: '12. 3. 2022',
