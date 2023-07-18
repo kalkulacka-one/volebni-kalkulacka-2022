1c2c9aa7fa3e9a092200db11a675c8096cbecd94
Kryštof Korb <krystof@korb.cz>
2023-07-18 12:49:39 +0200

    Update default files

--- frontend/cypress/e2e/example.cy.ts
+++ frontend/cypress/e2e/example.cy.ts
@@ -1,7 +1,8 @@
-// https://docs.cypress.io/api/introduction/api.html
+// https://on.cypress.io/api
 
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/');
+    // cy.contains('h1', 'You did it!');
   });
 });
