031349c8dd47ac9f97dbadf44d2552351ecd49a4
Kryštof Korb <krystof@korb.cz>
2023-08-17 21:34:21 +0200

    Drop manually imported types

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,16 +1,10 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-//@ts-ignore
 import { createI18n, useI18n } from 'vue-i18n';
-import type {
-  createI18n as createI18nType,
-  useI18n as useI18nType,
-} from 'vue-i18n/dist/vue-i18n.d.ts';
 import cs from './locales/cs.json';
 import sk from './locales/sk.json';
 
 type MessageSchema = typeof cs;
 
-export const i18n = (createI18n as typeof createI18nType)<
+export const i18n = (createI18n)<
   [MessageSchema],
   'cs' | 'sk'
 >({
@@ -23,9 +17,6 @@ export const i18n = (createI18n as typeof createI18nType)<
     sk,
   },
 });
-
-// this is here only to force the right typings
-export const useI18nWrapper = useI18n as typeof useI18nType;
 
 export const switchLanguage = async (newLocale: string) => {
   i18n.global.locale.value = newLocale;
