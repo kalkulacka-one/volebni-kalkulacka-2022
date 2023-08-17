f5d9c8353ac19a859d297b68557a667c1d8b7063
Kryštof Korb <krystof@korb.cz>
2023-08-17 21:37:58 +0200

    Run Prettier

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -4,10 +4,7 @@ import sk from './locales/sk.json';
 
 type MessageSchema = typeof cs;
 
-export const i18n = (createI18n)<
-  [MessageSchema],
-  'cs' | 'sk'
->({
+export const i18n = createI18n<[MessageSchema], 'cs' | 'sk'>({
   locale: import.meta.env.VITE_DEFAULT_LOCALE,
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
   legacy: false,
