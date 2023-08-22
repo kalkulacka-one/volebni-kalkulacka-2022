1bd375319482f60502ad9c99cdd764bc1490f293
Kryštof Korb <krystof@korb.cz>
2023-08-22 20:07:26 +0200

    Add transformations to fetch calculator function

--- frontend/src/common/dataFetch.ts
+++ frontend/src/common/dataFetch.ts
@@ -16,14 +16,137 @@ export const deprecatedFetchCalculator = async (
   electionId: string,
   districtId: string,
 ) => {
-  const calculator: DeprecatedCalculator = await fetch(
-    `/data/kalkulacka/${electionId}/${districtId}.json`,
-  ).then((x) => {
-    if (x.status === 200) {
-      return x.json();
+  let deprecatedCalculator = {} as DeprecatedCalculator;
+
+  deprecatedCalculator.district_code = 'inventura-2020-2023';
+  deprecatedCalculator.show_district_code = false;
+  deprecatedCalculator.name = 'Inventúra hlasovaní 2020–2023';
+  deprecatedCalculator.description =
+    'Volebná kalkulačka na základe skutočných hlasovaní z Národnej rady 2020–2023';
+  deprecatedCalculator.election = {
+    id: '57a09a41-631b-4b40-ac22-b75b8ce14d84',
+    key: 'nrsr-2023',
+    name: 'Parlamentné voľby 2023',
+    description:
+      'Voľby do Národnej rady Slovenskej republiky 30. septembra 2023.',
+  };
+
+  // Load questions and map them to deprecated structure
+  const questions = await fetchCalculatorQuestions();
+  const transformedQuestions = questions.map(
+    (question) =>
+      ({
+        id: question.id,
+        name: question.title, // Fill this with appropriate value
+        title: question.statement,
+        gist: question.detail,
+        detail: '',
+        tags: [],
+      }) as DeprecatedQuestion,
+  );
+  deprecatedCalculator.questions = transformedQuestions;
+
+  // Load candidates and map them to deprecated structure
+  const candidates = await fetchCalculatorCandidates();
+  const persons = await fetchCalculatorPersons();
+  const organizations = await fetchCalculatorOrganizations();
+  const transformedCandidates = candidates.map((candidate) => {
+    const transformedCandidate = {
+      id: candidate.id,
+      is_active: true,
+    } as DeprecatedCandidate;
+
+    if (candidate.reference.type == 'person') {
+      const person =
+        persons.find((person) => person.id === candidate.reference.id) ||
+        (() => {
+          throw new Error(
+            `Person with ID \`${candidate.reference.id}\` not found.`,
+          );
+        })();
+
+      transformedCandidate.name =
+        person.name || `${person.givenName} ${person.familyName}`;
+      transformedCandidate.given_name = person.givenName;
+      transformedCandidate.family_name = person.familyName;
+      transformedCandidate.type = 'person';
+      transformedCandidate.short_name = transformedCandidate.name;
+
+      const organizationId = person.memberOf?.[0]?.id;
+      if (!organizationId) {
+        throw new Error(
+          `Person with ID \`${candidate.reference.id}\` has no organization.`,
+        );
+      }
+      const organization =
+        organizations.find(
+          (organization) => organization.id === organizationId,
+        ) ||
+        (() => {
+          throw new Error(
+            `Organization with ID \`${organizationId}\` not found.`,
+          );
+        })();
+
+      transformedCandidate.parties = [
+        {
+          id: organization.id,
+          name: organization.name,
+          short_name: organization.shortName,
+          description: '',
+        },
+      ];
+    } else if (candidate.reference.type == 'organization') {
+      const organization =
+        organizations.find(
+          (organization) => organization.id === candidate.reference.id,
+        ) ||
+        (() => {
+          throw new Error(
+            `Organization with ID \`${candidate.reference.id}\` not found.`,
+          );
+        })();
+
+      transformedCandidate.name = organization.name;
+      transformedCandidate.type = 'party';
+      transformedCandidate.short_name = transformedCandidate.name;
     }
+    return transformedCandidate;
   });
-  return calculator;
+  deprecatedCalculator.candidates = transformedCandidates;
+
+  // Load candidates' answers and map them to deprecated structure
+  const candidatesAnswers = await fetchCalculatorCandidatesAnswers();
+  const transformedCandidatesAnswers = [] as DeprecatedCandidateAnswer[];
+  for (const candidateId in candidatesAnswers) {
+    const answers = candidatesAnswers[candidateId];
+    for (const answer of answers) {
+      const transformedAnswer =
+        answer.answer === true
+          ? 'yes'
+          : answer.answer === false
+          ? 'no'
+          : answer.answer === null
+          ? 'dont_know'
+          : (() => {
+              throw new Error(`Unexpected answer value: ${answer.answer}`);
+            })();
+
+      transformedCandidatesAnswers.push({
+        id: answer.questionId,
+        candidate_id: candidateId,
+        question_id: answer.questionId,
+        answer: transformedAnswer,
+      });
+    }
+  }
+  const ttransformedCandidatesAnswers = transformedCandidatesAnswers.filter(
+    (response) =>
+      response.candidate_id === '2de03a75-cf63-4fbf-a144-a6e6d71ae2c4',
+  );
+
+  deprecatedCalculator.answers = transformedCandidatesAnswers;
+  return deprecatedCalculator;
 };
 
 export const deprecatedFetchCalculators = async () => {
