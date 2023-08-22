36dabbe2c5ffdc34b282507517f1631c0655c708
Kryštof Korb <krystof@korb.cz>
2023-08-22 20:07:04 +0200

    Add hardcoded fetch functions

--- frontend/src/common/dataFetch.ts
+++ frontend/src/common/dataFetch.ts
@@ -1,4 +1,15 @@
-import type { DeprecatedCalculator } from '@/types/calculator';
+import type { CandidatesAnswers } from '@data/types/candidatesAnswers';
+import type { Candidates } from '@data/types/candidates';
+import type { Organizations } from '@data/types/organizations';
+import type { Persons } from '@data/types/persons';
+import type { Questions } from '@data/types/questions';
+
+import type { DeprecatedCandidate } from '@/types/candidate';
+import type {
+  DeprecatedCalculator,
+  DeprecatedCandidateAnswer,
+} from '@/types/calculator';
+import type { DeprecatedQuestion } from '@/types/question';
 import type { DeprecatedCalculators } from '@/types/calculators';
 
 export const deprecatedFetchCalculator = async (
@@ -53,4 +64,59 @@ export const deprecatedFetchElectionData = async (electionId: string) => {
     election: election,
     districts: districts,
   };
+};
+
+export const fetchCalculatorQuestions = async () => {
+  const data: Questions = await fetch(
+    '/data/instance/volebnakalkulacka.sk/nrsr-2023/inventura-2020-2023/questions.json',
+  ).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+  });
+  return data;
+};
+
+export const fetchCalculatorCandidatesAnswers = async () => {
+  const data: CandidatesAnswers = await fetch(
+    '/data/instance/volebnakalkulacka.sk/nrsr-2023/inventura-2020-2023/candidates-answers.json',
+  ).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+  });
+  return data;
+};
+
+export const fetchCalculatorCandidates = async () => {
+  const data: Candidates = await fetch(
+    '/data/instance/volebnakalkulacka.sk/nrsr-2023/inventura-2020-2023/candidates.json',
+  ).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+  });
+  return data;
+};
+
+export const fetchCalculatorPersons = async () => {
+  const data: Persons = await fetch(
+    '/data/instance/volebnakalkulacka.sk/nrsr-2023/inventura-2020-2023/persons.json',
+  ).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+  });
+  return data;
+};
+
+export const fetchCalculatorOrganizations = async () => {
+  const data: Organizations = await fetch(
+    '/data/instance/volebnakalkulacka.sk/nrsr-2023/inventura-2020-2023/organizations.json',
+  ).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+  });
+  return data;
 };
