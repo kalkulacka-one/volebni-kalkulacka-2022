7b443740a8b05a31a5180c4cd2e97224b7a60af8
Michal Škop <michal.skop@seznam.cz>
2023-08-24 16:19:17 +0200

    Add Slovak embeds (#569)

---
+++ frontend/src/components/embeds/AktualityEmbed.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ThemeProvider from '@/components/utilities/theming/ThemeProvider.vue';
+</script>
+
+<template>
+  <ThemeProvider theme="aktuality">
+    <router-view />
+  </ThemeProvider>
+</template>
---
+++ frontend/src/components/embeds/SmeEmbed.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ThemeProvider from '@/components/utilities/theming/ThemeProvider.vue';
+</script>
+
+<template>
+  <ThemeProvider theme="sme">
+    <router-view />
+  </ThemeProvider>
+</template>
