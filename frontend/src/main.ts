59ee8292468165e2234ffd057281b2d87ea7b9e8
Kryštof Korb <krystof@korb.cz>
2024-05-11 11:36:23 +0200

    Translate routes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -135,8 +135,8 @@ export const appRoutes = {
   },
   districtSelection: {
     name: 'district-selection',
-    path: '/volby/:election/vyber',
-    alias: '/volby/:election',
+    path: '/valasztasok/:election/vyber',
+    alias: '/valasztasok/:election',
     component: DistrictSelectionPageVue,
     meta: {
       title: 'Voksmonitor',
@@ -144,7 +144,7 @@ export const appRoutes = {
   },
   guide: {
     name: 'guide',
-    path: `/:type(${'kalkulacka'}|${'volby'})/:first/:second?/:third?/:fourth?/${'navod'}/:step(\\d+)?`,
+    path: `/:type(${'kalkulacka'}|${'valasztasok'})/:first/:second?/:third?/:fourth?/${'anleitung'}/:step(\\d+)?`,
     component: GuidePageVue,
     meta: {
       title: 'Návod - Voksmonitor',
@@ -152,7 +152,7 @@ export const appRoutes = {
   },
   question: {
     name: 'question',
-    path: `/:type(${'kalkulacka'}|${'volby'})/:first/:second?/:third?/:fourth?/${'otazka'}/:nr(\\d+)?`,
+    path: `/:type(${'kalkulacka'}|${'valasztasok'})/:first/:second?/:third?/:fourth?/${'frage'}/:nr(\\d+)?`,
     component: QuestionPageVue,
     meta: {
       title: 'Otázka $$ - Voksmonitor',
@@ -162,7 +162,7 @@ export const appRoutes = {
   },
   recap: {
     name: 'recap',
-    path: `/:type(${'kalkulacka'}|${'volby'})/:first/:second?/:third?/:fourth?/${'rekapitulacia'}/:nr(\\d+)?`,
+    path: `/:type(${'kalkulacka'}|${'valasztasok'})/:first/:second?/:third?/:fourth?/${'zusammenfassung'}/:nr(\\d+)?`,
     component: RecapPageVue,
     meta: {
       title: 'Rekapitulace - Voksmonitor',
@@ -170,7 +170,7 @@ export const appRoutes = {
   },
   result: {
     name: 'result',
-    path: `/:type(${'kalkulacka'}|${'volby'})/:first/:second?/:third?/:fourth?/${'vysledok'}`,
+    path: `/:type(${'kalkulacka'}|${'valasztasok'})/:first/:second?/:third?/:fourth?/${'ergebnisse'}`,
     component: ResultPageVue,
     meta: {
       title: 'Výsledky - Voksmonitor',
@@ -178,7 +178,7 @@ export const appRoutes = {
   },
   comparison: {
     name: 'comparison',
-    path: `/:type(${'kalkulacka'}|${'volby'})/:first/:second?/:third?/:fourth?/${'porovnaní'}`,
+    path: `/:type(${'kalkulacka'}|${'valasztasok'})/:first/:second?/:third?/:fourth?/${'vergleich'}`,
     component: ComparisonPageVue,
     meta: {
       title: 'Porovnaní - Voksmonitor',
