32fcbb1f01d1baaef0f1bea08e14c679a2ceb854
Michal Skop <michal.skop@kohovolit.eu>
2024-05-22 02:22:11 +0200

    new embed files Noen

---
+++ frontend/src/components/embeds/NoenEmbed.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ThemeProvider from '@/components/utilities/theming/ThemeProvider.vue';
+</script>
+
+<template>
+  <ThemeProvider theme="puls-24">
+    <router-view />
+  </ThemeProvider>
+</template>
