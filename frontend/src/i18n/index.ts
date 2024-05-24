b8c584b4a51e04aa4ff207bc88aa6e88c5374c40
Kryštof Korb <krystof@korb.cz>
2024-05-24 21:22:09 +0200

    Add English translations

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -2,10 +2,11 @@ import { createI18n, type Locale, type Composer } from 'vue-i18n';
 import cs from './locales/cs.json';
 import hu from './locales/hu.json';
 import sk from './locales/sk.json';
+import en from './locales/en.json';
 
 type MessageSchema = typeof cs;
 
-export const i18n = createI18n<[MessageSchema], 'cs' | 'hu' | 'sk'>({
+export const i18n = createI18n<[MessageSchema], 'cs' | 'hu' | 'sk' | 'en'>({
   locale: import.meta.env.VITE_DEFAULT_LOCALE,
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
   legacy: false,
@@ -13,6 +14,7 @@ export const i18n = createI18n<[MessageSchema], 'cs' | 'hu' | 'sk'>({
     cs,
     hu,
     sk,
+    en,
   },
 });
 
