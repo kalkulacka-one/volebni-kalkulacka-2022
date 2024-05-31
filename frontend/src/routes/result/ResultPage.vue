26df5d6b9c8d1b200d46b31f44cc3cb4bc1791d8
Kryštof Korb <krystof@korb.cz>
2024-05-31 16:31:56 +0200

    Switch to production

--- frontend/src/routes/result/ResultPage.vue
+++ frontend/src/routes/result/ResultPage.vue
@@ -342,7 +342,7 @@ onMounted(() => {
     document.addEventListener('VotematchEU-ready', function() {
       (window as any).VotematchEU.update({
         lang: voteMatchLanguage,
-        action:'https://acc-app.votematch.eu',
+        // action:'https://acc-app.votematch.eu',
       });
       (window as any).VotematchEU.results({
         country: voteMatchCountry,
