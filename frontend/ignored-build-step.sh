35be80c6a9cf113b46a4b99e59fb01409085d58f
Kryštof Korb <krystof@korb.cz>
2024-11-27 09:09:41 +0100

    Update ignored build step URL

--- frontend/ignored-build-step.sh
+++ frontend/ignored-build-step.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_PROJECT_PRODUCTION_URL: $VERCEL_PROJECT_PRODUCTION_URL"
 
-if [[ "$VERCEL_PROJECT_PRODUCTION_URL" == "www.volebnikalkulacka.cz" ]] ; then
+if [[ "$VERCEL_PROJECT_PRODUCTION_URL" == "www.testvot.eu" ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
