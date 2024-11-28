1bc1756b2a06984bfab77b9ec0418775bdc728b3
Mihály Hideg <hidegmisi@gmail.com>
2024-11-28 11:47:24 +0100

    change default lang to romanian

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,14 +1,16 @@
 import { createI18n, type Locale, type Composer } from 'vue-i18n';
 import cs from './locales/cs.json';
+import ro from './locales/ro.json';
 import sk from './locales/sk.json';
 
-type MessageSchema = typeof cs;
+type MessageSchema = typeof ro | typeof cs | typeof sk;
 
-export const i18n = createI18n<[MessageSchema], 'cs' | 'sk'>({
+export const i18n = createI18n<[MessageSchema], 'ro' | 'cs' | 'sk'>({
   locale: import.meta.env.VITE_DEFAULT_LOCALE,
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
   legacy: false,
   messages: {
+    ro,
     cs,
     sk,
   },
