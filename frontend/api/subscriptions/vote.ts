3e334071141202851afd4763b5a04e43653bec01
martinwenisch <martin@muhu.digital>
2024-11-28 13:36:46 +0100

    temp remove subscription

--- frontend/api/subscriptions/vote.ts
+++ frontend/api/subscriptions/vote.ts
@@ -1,25 +1,26 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-import { prisma } from '../../src/server/prisma';
 import { respond401, respond405 } from '../../src/server/errors';
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
-    const { email, vote, calculatorId } = req.body;
+    const { vote, calculatorId } = req.body;
 
     if (!vote || !calculatorId) {
       return respond401(res, 'Email, vote, and calculatorId are required.');
     }
 
-    const subscriberVote = await prisma.subscriberVote.create({
-        data: {
-            subEmail: email,
-            vote,
-            calculatorId,
-        },
-    });
+    console.info(`Vote: ${vote}, calculatorId: ${calculatorId}`);
 
-    return res.status(201).json(subscriberVote);
+    // const subscriberVote = await prisma.subscriberVote.create({
+    //     data: {
+    //         subEmail: email,
+    //         vote,
+    //         calculatorId,
+    //     },
+    // });
+
+    return res.status(201);
   } else {
     return respond405(res, req.method);
   }
