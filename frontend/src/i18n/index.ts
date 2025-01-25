15bc7323af06c48e90daeebc358048aa86816715
Kryštof Korb <krystof@korb.cz>
2025-01-25 12:41:44 +0100

    Add Albanian locale

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,16 +1,18 @@
 import { createI18n, type Locale, type Composer } from 'vue-i18n';
 import cs from './locales/cs.json';
 import sk from './locales/sk.json';
+import sq from './locales/sq.json';
 
 type MessageSchema = typeof cs;
 
-export const i18n = createI18n<[MessageSchema], 'cs' | 'sk'>({
+export const i18n = createI18n<[MessageSchema], 'cs' | 'sk' | 'sq'>({
   locale: import.meta.env.VITE_DEFAULT_LOCALE,
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
   legacy: false,
   messages: {
     cs,
     sk,
+    sq,
   },
 });
 
