447b43790966414c4eadbfe2fbc4f8fb665c75a3
Kryštof Korb <krystof@korb.cz>
2025-09-12 12:33:14 +0200

    Remove all database

--- frontend/src/components/design-system/navigation/NavigationBar.vue
+++ frontend/src/components/design-system/navigation/NavigationBar.vue
@@ -14,7 +14,6 @@ import BodyText from '@/components/design-system/typography/BodyText.vue';
 import EmbedWrapper from '@/components/utilities/embedding/EmbedWrapper.vue';
 import ResponsiveWrapper from '@/components/utilities/ResponsiveWrapper.vue';
 
-import type { User } from '@/stores/userStore';
 
 import { useI18n } from 'vue-i18n';
 
@@ -23,8 +22,6 @@ export interface Props {
   paddingResponsive?: boolean;
   transparent?: boolean;
   centeredTitle?: boolean;
-  user?: User | null;
-  withAccount?: boolean;
   withLogo?: boolean;
 }
 
@@ -35,7 +32,6 @@ const props = withDefaults(defineProps<Props>(), {
   paddingResponsive: true,
   transparent: false,
   centeredTitle: false,
-  user: undefined,
   withLogo: true,
 });
 
@@ -51,10 +47,6 @@ const background = computed(() =>
 
 const router = useRouter();
 
-const handleRegisterClick = () => router.push(appRoutes.register);
-
-const handleLoginClick = () => router.push(appRoutes.login);
-const handleAvatarClick = () => router.push(appRoutes.profile);
 </script>
 
 <template>
@@ -113,45 +105,6 @@ const handleAvatarClick = () => router.push(appRoutes.profile);
       spacing-responsive
     >
       <slot name="right">
-        <AvatarComponent
-          v-if="withAccount && user"
-          size="small"
-          background-color="rgb(var(--palette-primary-90))"
-          :name="user.displayName ? user.displayName : user.email"
-          class="user-avatar"
-          @click="() => handleAvatarClick()"
-        />
-        <StackComponent
-          v-if="withAccount && !user"
-          class="right"
-          horizontal
-          centered
-          spacing="medium"
-        >
-          <!-- <ResponsiveWrapper medium large extra-large huge>
-            <ButtonComponent kind="link" @click="handleRegisterClick">{{
-              $t(
-                'components.design-system.navigation.NavigationBar.create-account',
-              )
-            }}</ButtonComponent>
-            <ButtonComponent
-              kind="outlined"
-              size="small"
-              @click="handleLoginClick"
-              >{{
-                $t('components.design-system.navigation.NavigationBar.login')
-              }}</ButtonComponent
-            >
-          </ResponsiveWrapper>
-          <ResponsiveWrapper extra-small small>
-            <ButtonComponent
-              kind="link"
-              color="primary"
-              size="small"
-              @click="handleLoginClick"
-            ></ButtonComponent>
-          </ResponsiveWrapper> -->
-        </StackComponent>
       </slot>
     </StackComponent>
   </ContainerComponent>
