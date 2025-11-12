bce9883715cc66dcca2f0b4fcaf7ebe53a3c54d1
Kryštof Korb <krystof@korb.cz>
2025-11-12 13:42:26 +0100

    Update Vercel build command to work without database in Volební kalkulačka (CZ) archive 2024 (#754)

--- frontend/vercel-build.sh
+++ frontend/vercel-build.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 if [[ $VERCEL_ENV == "production" ]]; then
-  echo "Building for production environment, running database migration"
-  prisma generate && prisma migrate deploy && npm run build-only && cp -r ../data ./dist/
-elif [[ $VERCEL_GIT_COMMIT_REF == "staging" ]]; then
-  echo "Building for preview environment, running database migration for \`staging\` branch"
-  export DATABASE_URL="$DATABASE_URL_BASE/staging"
-  prisma generate && prisma migrate deploy && npm run build-only:preview && cp -r ../data ./dist/
+  echo "Building for production environment"
+  npm run build-only && cp -r ../data ./dist/
 else
-  echo "Building for preview environment, using a clean database for \`$VERCEL_GIT_COMMIT_REF\` branch"
-  GIT_COMMIT_REF_SHA=$(echo -n $VERCEL_GIT_COMMIT_REF | openssl dgst -sha256 | sed 's/^.* //')
-  export DATABASE_URL="$DATABASE_URL_BASE/$GIT_COMMIT_REF_SHA"
-  prisma generate && prisma migrate reset --force && npm run build-only:preview && cp -r ../data ./dist/
+  echo "Building for preview environment"
+  npm run build-only:preview && cp -r ../data ./dist/
 fi
